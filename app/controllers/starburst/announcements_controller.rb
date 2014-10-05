defba66b3fdb10733cf783695d3c1fe9f57625d9
Corey Martin <coreym@gmail.com>
2014-10-05 11:15:27 -0400

    announcements controller cleanup

--- app/controllers/starburst/announcements_controller.rb
+++ app/controllers/starburst/announcements_controller.rb
@@ -1,8 +1,9 @@
 module Starburst
 	class AnnouncementsController < ApplicationController
 		def mark_as_read
-			if current_user and Announcement.find(params[:id].to_i)
-				if AnnouncementView.create(:user => current_user, :announcement => Announcement.find(params[:id].to_i))
+			announcement = Announcement.find(params[:id].to_i)
+			if current_user && announcement
+				if AnnouncementView.create(:user => current_user, :announcement => announcement)
 					render :json => :ok
 					return
 				end
