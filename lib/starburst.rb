c919e71d1fae442257a771bf47f9eb60ecb5597b
Corey Martin <coreym@gmail.com>
2014-10-06 09:12:08 -0400

    gem config

--- lib/starburst.rb
+++ lib/starburst.rb
@@ -1,4 +1,9 @@
 require "starburst/engine"
+require "helpers/configuration"
 
 module Starburst
+	extend Configuration
+
+	define_setting :current_user_class, "current_user"
+	define_setting :user_instance_methods
 end
