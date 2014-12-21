026ea3f0cc35337261152b6bcf9efb6a5e339ca3
Corey Martin <coreym@gmail.com>
2014-12-21 17:47:07 -0500

    fix bug where announcement past first can't be marked as read

--- lib/starburst/version.rb
+++ lib/starburst/version.rb
@@ -1,3 +1,3 @@
 module Starburst
-  VERSION = "0.9.3"
+  VERSION = "1.0.0"
 end
--- spec/controllers/starburst/announcements_controller_spec.rb
+++ spec/controllers/starburst/announcements_controller_spec.rb
@@ -6,14 +6,19 @@ module Starburst
 		
 		routes { Starburst::Engine.routes } # http://pivotallabs.com/writing-rails-engine-rspec-controller-tests/
 
-		it "marks an announcement as read" do
+		it "marks an announcement as read (twice)" do
 			@current_user = mock_model(User, :id => 10)
 			controller.stub(:current_user).and_return(@current_user)
 			announcement = FactoryGirl.create(:announcement)
+			announcement2 = FactoryGirl.create(:announcement)
 			post :mark_as_read, :id => announcement.id
 			expect(response.status).to eq 200
 			expect(AnnouncementView.last.user_id).to eq 10
 			expect(AnnouncementView.all.length).to eq 1
+			post :mark_as_read, :id => announcement2.id
+			expect(response.status).to eq 200
+			expect(AnnouncementView.last.user_id).to eq 10
+			expect(AnnouncementView.all.length).to eq 2
 		end
 		
 		it "does not mark an announcement as read if no one is logged in" do
