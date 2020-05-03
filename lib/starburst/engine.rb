637c72d4d0d5c88b6aa2b6d9dab12ddd8763dd50
Erich Soares Machado <erichmachado@users.noreply.github.com>
2020-05-03 12:27:42 -0400

    Updates engine settings

--- lib/starburst/engine.rb
+++ lib/starburst/engine.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
 module Starburst
   class Engine < ::Rails::Engine
     isolate_namespace Starburst
 
     config.generators do |g|
-      g.test_framework      :rspec,        :fixture => false
-      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_bot
       g.assets false
       g.helper false
     end
-
   end
 end
