5ac7c52c63706a6e06ab37049f728afaa4aa220d
Erich Soares Machado <erichmachado@users.noreply.github.com>
2020-05-03 20:34:32 -0400

    Include private and protected methods when searching for Starburst.current_user_method

--- app/controllers/starburst/announcements_controller.rb
+++ app/controllers/starburst/announcements_controller.rb
@@ -4,7 +4,7 @@ module Starburst
 	class AnnouncementsController < Starburst.base_controller.constantize
 		def mark_as_read
 			announcement = Announcement.find(params[:id].to_i)
-			if respond_to?(Starburst.current_user_method) && send(Starburst.current_user_method) && announcement
+			if respond_to?(Starburst.current_user_method, true) && send(Starburst.current_user_method) && announcement
 				if AnnouncementView.where(user_id: send(Starburst.current_user_method).id, announcement_id: announcement.id).first_or_create(user_id: send(Starburst.current_user_method).id, announcement_id: announcement.id)
 					render :json => :ok
 				else
