1990fa48124ce4298eee24a79576d53ae17ab4f6
Erich Soares Machado <erichmachado@users.noreply.github.com>
2020-05-06 21:24:34 -0400

    Cleans up RSpec's Rails helper script

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,12 +4,8 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('dummy/config/environment.rb',  __dir__)
-# Prevent database truncation if the environment is production
-abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'factory_bot_rails'
-require 'capybara'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
