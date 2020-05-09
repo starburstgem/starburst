e15eb761ddd97018c0e6a9eef82096c2bc3f9be5
Erich Soares Machado <erichmachado@users.noreply.github.com>
2020-05-09 12:33:17 -0400

    Removes engine generator settings

--- lib/starburst/engine.rb
+++ lib/starburst/engine.rb
@@ -3,12 +3,5 @@
 module Starburst
   class Engine < ::Rails::Engine
     isolate_namespace Starburst
-
-    config.generators do |g|
-      g.test_framework :rspec, fixture: true
-      g.fixture_replacement :factory_bot
-      g.assets false
-      g.helper false
-    end
   end
 end
