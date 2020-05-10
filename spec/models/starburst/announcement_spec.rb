2abd25b738e70e5d11ad3ea16276d11731a432ea
Erich Soares Machado <erichmachado@users.noreply.github.com>
2020-05-10 16:36:18 -0400

    Refactors Starburst::Announcement.find_announcement_for_current_user specs

--- spec/models/starburst/announcement_spec.rb
+++ spec/models/starburst/announcement_spec.rb
@@ -55,66 +55,61 @@ RSpec.describe Starburst::Announcement do
     it { is_expected.to contain_exactly(announcement1) }
   end
 
-  context "an announcement targeted to certain users" do
-    it "has a limited_to_users field that is retrieveable from the database" do
-      limited_announcement = Announcement.create(:body => "test", :limit_to_users => [
-        {
-          :field => "subscription",
-          :value => "",
-          :operator => "="
-        }
-      ])
-      expect(limited_announcement.limit_to_users[0][:field]).to eq "subscription"
+  describe '.find_announcement_for_current_user' do
+    subject { described_class.find_announcement_for_current_user(described_class.all, user) }
+
+    context 'with an attribute condition' do
+      let!(:announcement) do
+        create(
+          :announcement,
+          limit_to_users: [
+            {
+              field: 'subscription',
+              value: 'weekly'
+            }
+          ]
+        )
+      end
+
+      context 'when the user should see the announcement' do
+        let(:user) { create(:user, subscription: 'weekly') }
+
+        it { is_expected.to eq(announcement) }
+      end
+
+      context 'when the user should not see the announcement' do
+        let(:user) { create(:user, subscription: 'monthly') }
+
+        it { is_expected.to be_nil }
+      end
     end
 
-    it "shows up for the proper user only (one positive condition)" do
-      limited_announcement = Announcement.create(:body => "test", :limit_to_users => [
-        {
-          :field => "subscription",
-          :value => ""
-        }
-      ])
-      user_who_should_see_announcement = create(:user, :subscription => "")
-      user_who_should_not_see_announcement = create(:user, :subscription => "monthly")
-      expect(Announcement.find_announcement_for_current_user(Announcement.where(nil), user_who_should_see_announcement)).to eq limited_announcement
-      expect(Announcement.find_announcement_for_current_user(Announcement.where(nil), user_who_should_not_see_announcement)).to eq nil
+    context 'with a method condition' do
+      let!(:announcement) do
+        create(
+          :announcement,
+          limit_to_users: [
+            {
+              field: 'free?',
+              value: true
+            }
+          ]
+        )
+      end
+
+      before { allow(Starburst).to receive(:user_instance_methods).and_return(%i[free?]) }
+
+      context 'when the user should see the announcement' do
+        let(:user) { create(:user, subscription: '') }
+
+        it { is_expected.to eq(announcement) }
+      end
+
+      context 'when the user should not see the announcement' do
+        let(:user) { create(:user, subscription: 'monthly') }
+
+        it { is_expected.to be_nil }
+      end
     end
-
-    it "shows up for the proper user only (method condition)" do
-      allow(Starburst).to receive(:user_instance_methods).and_return(%i[free?])
-      limited_announcement = Announcement.create(:body => "test", :limit_to_users => [
-        {
-          :field => "free?",
-          :value => true
-        }
-      ])
-      user_who_should_see_announcement = create(:user, :subscription => "")
-      user_who_should_not_see_announcement = create(:user, :subscription => "monthly")
-      expect(Announcement.find_announcement_for_current_user(Announcement.where(nil), user_who_should_see_announcement)).to eq limited_announcement
-      expect(Announcement.find_announcement_for_current_user(Announcement.where(nil), user_who_should_not_see_announcement)).to eq nil
-    end
-
-    # it "performs" do
-    #   pending
-    #   require 'benchmark'
-
-    #   (1 .. 500).each do
-    #     Announcement.create(:limit_to_users => [
-    #       {
-    #         :field => "subscription",
-    #         :value => ""
-    #       }
-    #     ])
-    #   end
-
-    #   (1 .. 10000).each do
-    #     create(:user, :subscription => "")
-    #   end
-
-    #   Benchmark.realtime{
-    #   Announcement.current(User.last)
-    #   }
-    # end
-
   end
 end
