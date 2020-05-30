12b2360c0f157f434f172119de92029fb0b910f1
Erich Soares Machado <erichmachado@users.noreply.github.com>
2020-05-30 15:22:57 -0400

    Refactors Announcements feature specs

--- spec/features/announcements_spec.rb
+++ spec/features/announcements_spec.rb
@@ -1,28 +1,31 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-feature 'Announcements' do
-	scenario 'show announcement to user' do
-		@current_user = create(:user)
-		create(:announcement, :body => "My announcement")
-		visit root_path(user_id: @current_user.id)
-		page.should have_content "My announcement"
-	end
-	scenario 'show no announcements if user not logged in' do
-		create(:announcement, :body => "My announcement")
-		visit root_path
-		page.should_not have_content "My announcement"
-		page.should have_content "Sample homepage"
-	end
-	scenario 'stop showing announcement once the user has read it' do
-		@current_user = create(:user)
-		announcement = create(:announcement, :body => "My announcement")
-		visit root_path(user_id: @current_user.id)
-		page.should have_content "My announcement"
-		create(:announcement_view, :user => @current_user, :announcement => announcement)
-		visit root_path(user_id: @current_user.id)
-		page.should_not have_content "My announcement"
-	end
-	scenario 'allow the user to click to hide the announcement' do
-		skip "Figure out how to best stub the current_user method for a JavaScript-enabled test"
-	end
+RSpec.describe 'Announcements' do
+  let!(:announcement) { create(:announcement) }
+
+  context 'when the User is signed in' do
+    let(:current_user) { create(:user) }
+
+    before { visit root_path(user_id: current_user.id) }
+
+    it 'displays the current Announcement' do
+      expect(page).to have_content('Sample homepage')
+      expect(page).to have_content(announcement.body)
+    end
+
+    skip 'hides the Announcement if the User closes it', js: true, driver: :selenium_chrome_headless do
+      expect(page).to have_content(announcement.body)
+      find('#starburst-close').click
+      expect(page).not_to have_content(announcement.body)
+    end
+  end
+
+  context 'when the User is not signed in' do
+    before { visit root_path }
+
+    it 'does not display the current Announcement' do
+      expect(page).to have_content('Sample homepage')
+      expect(page).not_to have_content(announcement.body)
+    end
+  end
 end
