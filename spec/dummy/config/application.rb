858193e5e331dcebb0a552ad7413678cd05b30c2
Erich Soares Machado <erichmachado@users.noreply.github.com>
2020-05-31 10:37:49 -0400

    Removes action_mailer/railtie from dummy app

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -4,7 +4,6 @@ require 'rails'
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_view/railtie'
-require 'action_mailer/railtie'
 
 Bundler.require(:default, :development)
 
